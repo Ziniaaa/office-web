dbb931ea812ba1c35955b4e5fbd3566c1aff159e
Ziniaaa <ziniania0727@gmail.com>
2025-01-04 21:05:35 +0800

    0104

--- js/script.js
+++ js/script.js
@@ -1,13 +1,27 @@
+// JQuery
+$(function () {
+    // 漢堡按鈕
+    $('.hamburger').click(function () {
+        $(this).toggleClass('is-active');
+        $('#menuOverlay-right').toggleClass('show');
+    });
 
-const divBelow = document.getElementById('mainLeft');
-const menuOverlay = document.getElementById('menuOverlay-left');
+    $('.ham-inside').click(function () {
+        $(this).toggleClass('is-active');
+        $('#menuOverlay-right').toggleClass('show');
+    });
+})
 
-function updatemenuOverlayWidth() {
-    menuOverlay.style.width = `${divBelow.offsetWidth}px`;
-}
+// const divBelow = document.getElementById('mainLeft');
+// const menuOverlay = document.getElementById('menuOverlay-left');
 
-// 初始化
-updatemenuOverlayWidth();
+// function updatemenuOverlayWidth() {
+//     menuOverlay.style.width = `${divBelow.offsetWidth}px`;
+// }
 
-// 當窗口大小改變時自動更新
-window.addEventListener('resize', updatemenuOverlayWidth);
+// // 初始化
+// updatemenuOverlayWidth();
+
+// // 當窗口大小改變時自動更新
+// window.addEventListener('resize', updatemenuOverlayWidth);
+
---
+++ js/script_index.js
@@ -0,0 +1,30 @@
+// 首頁輪播套件配置
+var timelineSwiper = new Swiper('.timeline .swiper-container', {
+    direction: 'vertical',
+    loop: true,
+    speed: 1600,
+    // Swiper 3.4.2 的自動播放設置
+    autoplay: 5000, // 設置停留時間為 5 秒
+    autoplayDisableOnInteraction: false,
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    paginationBulletRender: function (swiper, index, className) {
+        var num = document.querySelectorAll('.swiper-slide')[index].getAttribute('data-num');
+        return '<span class="' + className + '">' + num + '</span>';
+    },
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    breakpoints: {
+        768: {
+            direction: 'horizontal',
+        }
+    }
+});
+
+// 為活動的幻燈片添加動畫
+timelineSwiper.on('slideChangeStart', function () {
+    var activeSlide = document.querySelectorAll('.swiper-slide')[this.activeIndex];
+    if (activeSlide) {
+        activeSlide.classList.add('swiper-slide-active');
+    }
+});
